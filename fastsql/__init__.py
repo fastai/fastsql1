a5b1c4f062f424328ea037b68fea16518572fc5b
Jeremy Howard <info@fast.ai>
2019-07-22 16:23:53 +0000

    sql statements; better setup

--- fastsql/__init__.py
+++ fastsql/__init__.py
@@ -1,25 +1,29 @@
 import sqlalchemy, pandas as pd
-from sqlalchemy import create_engine,MetaData,Table,Column
-from sqlalchemy.engine import url
+from sqlalchemy import create_engine,MetaData,Table,Column,engine,sql
 from sqlalchemy.sql.base import ImmutableColumnCollection
 
 __all__ = 'pd conn_db patch'.split()
 
 def conn_db(drivername, username=None, password=None, host=None, port=None, database=None):
-    engine = create_engine(url.URL(drivername, username, password, host, port, database=database))
-    engine.connect()
-    meta = MetaData(bind=engine)
+    "Connect to DB using `url.URL()` params and return filled `MetaData`"
+    eng= create_engine(engine.url.URL(drivername, username, password, host, port, database=database))
+    eng.connect()
+    meta = MetaData(bind=eng)
     meta.reflect()
     return meta
 
 def patch(f):
-    n = f.__name__
+    "Decorator to patch `f` into class of f's first param type annotation"
     cls = next(iter(f.__annotations__.values()))
-    old_f = getattr(cls, n, None)
-    def _inner(self, *args, **kwargs):
-        return f(self, old_f, *args, **kwargs) if old_f else f(self, *args, **kwargs)
-    setattr(cls,n,_inner)
+    old_f = getattr(cls, f.__name__, None)
+    def _f(o, *a, **k): return f(o, old_f, *a, **k) if old_f else f(o, *a, **k)
+    setattr(cls,f.__name__,_f)
     return f
+
+@patch
+def __getattr__(self:MetaData, n):
+    if n in self.tables: return self.tables[n],self.tables[n].c
+    raise AttributeError
 
 @patch
 def __dir__(self:ImmutableColumnCollection, old_f): return old_f(self) + self.keys()
@@ -27,12 +31,29 @@ def __dir__(self:ImmutableColumnCollection, old_f): return old_f(self) + self.ke
 def __dir__(self:MetaData,                  old_f): return old_f(self) + list(self.tables)
 
 @patch
-def __getattr__(self:MetaData, n):
-    if n is None or n not in self.tables: raise AttributeError
-    res = self.tables[n]
-    return res,res.c
+def sql(self:engine.Engine, statement, *args, **kwargs):
+    "Execute `statement` string and return `DataFrame` of results (if any)"
+    t = self.execute(statement, *args, **kwargs)
+    if not t.cursor: return
+    return pd.DataFrame(t.fetchall(), columns=t.keys())
+
+@patch
+def sql(self:MetaData, statement, *args, **kwargs):
+    "Execute `statement` string and return `DataFrame` of results (if any)"
+    return self.bind.sql(statement, *args, **kwargs)
+
+@patch
+def df(self:engine.Engine, cmd):
+    "Execute SqlAlchemy `cmd` and return `DataFrame` of results"
+    return pd.read_sql(cmd, self)
+
+@patch
+def df(self:MetaData, cmd):
+    "Execute SqlAlchemy `cmd` and return `DataFrame` of results"
+    return self.bind.df(cmd)
 
 @patch
 def df(self:Table, where=None, limit=None):
-    return pd.read_sql(self.select(where).limit(limit), self.bind)
+    "`DataFrame` of table, optitionally limited by `where` and `limit` clauses"
+    return self.bind.df(self.select(where).limit(limit))
 
--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+import os
 
-setup(name='fastsql', version='0.1',
-      description='A bit of extra usability for sqlalchemy',
-      url='http://github.com/jph00/fastsql',
-      author='Jeremy Howard', author_email='info@fast.ai',
-      license='Apache 2.0', packages=['fastsql'], zip_safe=False)
+owner,name = 'jph00','fastsql'
+exec(open(os.path.join(name, 'version.py')).read())
+readme = open('README.md').read()
+
+setup(description='A bit of extra usability for sqlalchemy',
+      author='Jeremy Howard', author_email='info@fast.ai', license='Apache 2.0',
+      url='http://github.com/'+owner+'/'+name,
+      python_requires  = '>=3.6', install_requires = ['sqlalchemy', 'pandas'],
+      long_description = readme, long_description_content_type = 'text/markdown',
+      name=name, version=__version__, packages=find_packages(), zip_safe=False)
 
